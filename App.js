78ffd02c6788c51ea8c2d8c1602071af57c75672
Arief Yusron <ariefyusron98@gmail.com>
2019-02-22 03:02:51 +0700

    config root reducer and store

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
+import store from './src/redux/store';
 import Home from './src/screens/Home';
 
 class App extends Component {
 
   render() {
     return (
-      <Home />
+      <Provider store={store}>
+        <Home />
+      </Provider>
     );
   }
 }
