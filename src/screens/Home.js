4b21721718eb20c3cbffac98bbf105602e4d426f
Arief Yusron <ariefyusron98@gmail.com>
2019-02-22 03:52:55 +0700

    install native base and create button for add agenda

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 import { connect } from 'react-redux';
 
 import CalendarComponent from '../components/CalendarComponent';
@@ -14,15 +15,35 @@ class Home extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <CalendarComponent />
         <ModalComponent />
-        <TouchableOpacity onPress={() => this.handleVisibleModal(true)}>
-          <Text>Open Modal</Text>
+        <TouchableOpacity onPress={() => this.handleVisibleModal(true)} style={styles.button}>
+          <Icon name='plus' type='AntDesign' style={styles.iconButton} />
         </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  button: {
+    backgroundColor: '#2ecc71',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 30,
+    right: 30
+  },
+  iconButton: {
+    color: '#fff'
+  }
+})
 
 export default connect()(Home);
